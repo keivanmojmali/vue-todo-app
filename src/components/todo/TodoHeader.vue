be2d31b8c2ca26710a312169166f079fac858770
Keivan <mojmalik@gmail.com>
2025-08-07 11:45:16 -0400

    adding stuff

--- src/components/todo/TodoHeader.vue
+++ src/components/todo/TodoHeader.vue
@@ -5,14 +5,21 @@
 
 
 <template>
-  <h1>helllllloooooo</h1>
-  <div class="md:flex md:items-center md:justify-between">
-    <div class="min-w-0 flex-1">
-      <h2 class="text-2xl/7 font-bold text-white sm:truncate sm:text-3xl sm:tracking-tight">Keivan's
-      Todo App</h2>
-    </div>
-    <div class="mt-4 flex md:ml-4 md:mt-0">
-    </div>
+  <div class="header">
+    <h1 class="title">Keivan's Todo App</h1>
   </div>
 </template>
 
+<style scoped>
+
+.title {
+  padding-left: 10px;
+  color: white;
+  font-size: 2rem;
+}
+
+.header {
+  background-color: black
+}
+
+</style>
