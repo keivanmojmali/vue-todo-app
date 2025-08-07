9ee7f4cfa88593b588a3bc6961f804773ef86525
Keivan <mojmalik@gmail.com>
2025-08-07 11:28:25 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import TodoHeader from "@/components/todo/TodoHeader.vue"
+</script>
+
+<template>
+  <TodoHeader />
+  <h1>this is going to be the main page</h1>
+</template>
---
+++ src/components/todo/TodoHeader.vue
@@ -0,0 +1,18 @@
+
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+  <h1>helllllloooooo</h1>
+  <div class="md:flex md:items-center md:justify-between">
+    <div class="min-w-0 flex-1">
+      <h2 class="text-2xl/7 font-bold text-white sm:truncate sm:text-3xl sm:tracking-tight">Keivan's
+      Todo App</h2>
+    </div>
+    <div class="mt-4 flex md:ml-4 md:mt-0">
+    </div>
+  </div>
+</template>
+
